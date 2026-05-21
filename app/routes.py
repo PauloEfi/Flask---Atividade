2b7d6d20fa2f9627aa2f34526029b420ef04dfe5
PauloEfi <paulofelipe977@gmail.com>
2026-05-21 17:19:29 -0300

    1234567

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,14 @@
 from app import app
-from flask import render_template
+from flask import render_template [cite: 55, 56]
+
 @app.route('/')
-def homepage ():
-    return render_template('index.html')
+def homepage():
+    return render_template('index.html') [cite: 57, 58, 59]
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html') [cite: 107]
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html') [cite: 107]
